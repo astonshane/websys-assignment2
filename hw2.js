47d211516662157812be002c98d4b8254eed2189
Kayla Cinnamon <cinnak@rpi.edu>
2015-10-19 16:55:59 -0400

    Added JS File

---
+++ hw2.js
@@ -0,0 +1,51 @@
+function hexFromRGB(r, g, b) {
+    var hex = [
+      r.toString( 16 ),
+      g.toString( 16 ),
+      b.toString( 16 )
+    ];
+    $.each( hex, function( nr, val ) {
+      if ( val.length === 1 ) {
+        hex[ nr ] = "0" + val;
+      }
+    });
+    return hex.join( "" ).toUpperCase();
+  }
+  function refreshSwatch() {
+    var red = $( "#red" ).slider( "value" ),
+      green = $( "#green" ).slider( "value" ),
+      blue = $( "#blue" ).slider( "value" ),
+      hex = hexFromRGB( red, green, blue );
+    $( "#swatch" ).css( "background-color", "#" + hex );
+  }
+  $(function() {
+    $( "#red, #green, #blue" ).slider({
+      orientation: "horizontal",
+      range: "min",
+      max: 255,
+      value: 127,
+      slide: refreshSwatch,
+      change: refreshSwatch
+    });
+    $( "#red" ).slider( "value", 255 );
+    $( "#green" ).slider( "value", 140 );
+    $( "#blue" ).slider( "value", 60 );
+  });
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  $( document ).ready(function() {
+    var red = getRandomInt(0,256);
+    var green = getRandomInt(0,256);
+    var blue = getRandomInt(0,256);
+    
+    console.log(red);
+    console.log(green);
+    console.log(blue);
+
+    hex = hexFromRGB( red, green, blue );
+    $( "#randomSwatch" ).css( "background-color", "#" + hex );
+
+  });
