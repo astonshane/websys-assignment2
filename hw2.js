ccfb06b282b9d1d261682ce765eb7444ff5362f6
Shane Aston <astonshane@gmail.com>
2015-10-19 17:26:49 -0400

    added some input fields

--- hw2.js
+++ hw2.js
@@ -59,3 +59,21 @@ function hexFromRGB(r, g, b) {
     $( "#randomSwatch" ).css( "background-color", "#" + hex );
 
   });
+
+
+
+  $(function() {
+    var select = $( "#minbeds" );
+    var slider = $( "<div id='slider'></div>" ).insertAfter( select ).slider({
+      min: 1,
+      max: 6,
+      range: "min",
+      value: select[ 0 ].selectedIndex + 1,
+      slide: function( event, ui ) {
+        select[ 0 ].selectedIndex = ui.value - 1;
+      }
+    });
+    $( "#minbeds" ).change(function() {
+      slider.slider( "value", this.selectedIndex + 1 );
+    });
+  });
