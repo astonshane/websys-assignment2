78171866614dbe453c36375d0a79508f09f83101
Shane Aston <astonshane@gmail.com>
2015-10-21 13:46:25 -0400

    broken scoring

--- hw2.js
+++ hw2.js
@@ -79,13 +79,11 @@ function hexFromRGB(r, g, b) {
   var r_off = (Math.abs(exp_r - act_r)/255)*100;
   var g_off = (Math.abs(exp_g - act_g)/255)*100;
   var b_off = (Math.abs(exp_b - act_b)/255)*100;
-  alert(r_off);
   var p_off = (r_off + g_off + b_off)/3
   var difficulty = 5;
   var msec_taken = 1;
   // var ans = 1;
   var ans = ((15 - difficulty - p_off) / (15 - difficulty)) * (15000 - msec_taken);
-  alert(ans);
   return ans;
   }
 
@@ -113,10 +111,9 @@ function hexFromRGB(r, g, b) {
     green_exp = rgb[2];
     blue_exp = rgb[3];
 
-    red_act = $( "#red" ).slider( "value", $("#redBox").val() );
-    green_act = $( "#green" ).slider( "value", $("#greenBox").val() );
-    blue_act = $( "#blue" ).slider( "value", $("#blueBox").val() );
-    //alert(Integer.parseInt(string(red_act)))
+    red_act = parseInt($("#redBox").val());
+    green_act = parseInt($("#greenBox").val());
+    blue_act = parseInt($("#redBox").val());
 
     ans = scoring(red_act, green_act, blue_act, red_exp, green_exp, blue_exp);
     alert(ans);
